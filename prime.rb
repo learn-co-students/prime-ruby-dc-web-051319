5e45f454b2c9169eb22ad894d2908b636ca49b9a
Kolton Starr <starrkolton@gmail.com>
2019-04-29 16:36:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def eratosthenes(n) #returns an array of all prime numbers
+  nums = [nil, nil, *2..n]
+  (2..Math.sqrt(n)).each do |i|
+    (i**2..n).step(i){|m| nums[m] = nil}  if nums[i]
+  end
+  nums.compact
+end
+
+def prime?(num)
+  if num < 0
+    false
+  elsif eratosthenes(num).include?(num)
+    true
+  else 
+    false
+  end 
+end 
