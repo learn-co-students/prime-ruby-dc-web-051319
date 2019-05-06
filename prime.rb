a2becbf89cd88468cf0d813437021eef9bc0c42e
Emily Bolten <emilybolten@gmail.com>
2019-05-06 14:29:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+
+def prime?(num)
+
+  if num > 0
+    (2..(num - 1)).each do |i|
+      return false if num % i == 0
+    end
+
+  elsif num < 0
+    (-2..(num + 1)).each do |i|
+      return false if num % i == 0
+    end
+  end
+
+  if num == 0 || num == -1 || num == 1
+    return false
+  end
+
+  return true
+
+end
